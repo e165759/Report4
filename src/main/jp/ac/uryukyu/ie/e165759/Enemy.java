92bd2b5d057a7fdf7a482bbe2e96da259b821653
Hiroyuki Tamae <e165759@ie.u-ryukyu.ac.jp>
2016-11-23 22:15:31 +0900

    Step.1 Fix : Enemy can attack the hero when enemy.dead is true.

--- src/main/jp/ac/uryukyu/ie/e165759/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e165759/Enemy.java
@@ -50,6 +50,9 @@ public class Enemy {
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
+        if(this.dead = true){
+            return;
+        }
         int damage = (int)(Math.random() * attack);
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
         hero.wounded(damage);
